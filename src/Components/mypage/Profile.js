b3e788d9accdd04212e5bc9cb9b0c27f35cdcaed
MCirilo1 <miguel.cirilo@yahoo.com>
2020-08-04 17:36:31 -0400

    adding bootstrap card

--- src/Components/mypage/Profile.js
+++ src/Components/mypage/Profile.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import ProfileArea from './ProfileArea';
+import propTypes from 'prop-types';
 
 
 class Profile extends React.Component {
         state = {
+            user: {
             firstName: 'Miguel',
             lastName: 'Cirilo',
+            avatar: 'https://avatars2.githubusercontent.com/u/64857551?s=460&v=4'
+            }
         }
     
     
     render() {
-        return this.state.firstName + ' ' + this.state.lastName
+        return (
+            <ProfileArea
+                profile={this.state.user}
+            />
+        )
     }
+}
+
+Profile.propTypes = {
+    user: propTypes.shape({
+        firstName: propTypes.string,
+        lastName: propTypes.string,
+        avatr: propTypes,
+    })
 }
 
 export default Profile;
--- src/Components/mypage/ProfileArea.js
+++ src/Components/mypage/ProfileArea.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import {Button} from 'reactstrap';
+
 
 
 function ProfileArea(props) {
-    const handleEditClick = () => {
-        props.handleEdit(props.Profile)
-    };
+   
 
     return (
-        <div>
-            <Button
-                onClick={handleEditClick}
-                ></Button>
+        <React.Fragment>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={props.profile.avatar} className="card-img-top" alt="avatarUrl" />
+          <div className="card-body">
+                <h5 className="card-title">{props.profile.firstName} {props.profile.lastName}</h5>
+                <p className="card-text">
+                 Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </p>
+          </div>
         </div>
+        </React.Fragment>
     )
 }
 
@@ -22,8 +27,9 @@ ProfileArea.propTypes = {
     profile: propTypes.shape({
         firstName: propTypes.string,
         lastName: propTypes.string,
+        avatar: propTypes,
     }),
-    handleEditClick: propTypes.func,
+    
 }
 
 export default ProfileArea;
